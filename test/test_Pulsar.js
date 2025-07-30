d3a990b9e06c79cd32961b857383870f1339a3da
c0d3runn3r <coderunner@novadynamics.com>
2025-07-29 20:57:19 -0700

    All working

--- test/test_Pulsar.js
+++ test/test_Pulsar.js
@@ -18,7 +18,7 @@ describe('Pulsar', function () {
         ss.add(item);
         pulsar.trigger();
         assert.strictEqual(result.length, 1);
-        assert.equal(result[0], '{"event_name":"added","item":{"id":"item1","type":"SynchroItem","notions":{},"properties":{}}}');
+        assert.deepStrictEqual(JSON.parse(result[0]), {"event_name":"added","item":{"id":"item1","type":"SynchroItem","notions":{},"properties":{}}});
     });
 
     it('should result in no events when an item is added and then removed', function () {
@@ -40,7 +40,69 @@ describe('Pulsar', function () {
         assert.deepStrictEqual(JSON.parse(result[1]), {"event_name":"changed","item":{"id":"item1"},"change":{"property":"testN","new_value":"value3","new_timestamp":"2025-07-28T16:47:05.000Z"}});
         assert.ok(typeof JSON.parse(result[1]).change.old_value == 'undefined'); // Because the initial value is undefined
 
-        //assert.strictEqual(result.length, 1);
+    });
+
+    it('adding multiple items and events, triggering, then removing one item shoud result in properly collapsed set', function(){
+
+        const item = new SynchroItem('item1');
+        const item2 = new SynchroItem('item2');
+
+        ss.add(item);    
+        ss.add(item2);
+        item.set('testN', 'value1', new Date("2025-07-28T16:47:03Z"));
+        item2.set('testN2', 'value1', new Date("2025-07-28T16:47:03Z"));
+
+        pulsar.trigger();
+
+        item.set('testN', 'value2', new Date("2025-07-28T16:47:04Z"));
+        item.set('testN', 'value3', new Date("2025-07-28T16:47:05Z"));
+
+        item2.set('testN2', 'value2', new Date("2025-07-28T16:47:04Z"));
+        item2.set('testN2', 'value3', new Date("2025-07-28T16:47:05Z"));
+        ss.remove(item);
+
+        pulsar.trigger();
+
+        // We should now only have item2, with a single change from value1 -> value3 and the removal of item1
+        assert.strictEqual(result.length, 2);
+        assert.deepStrictEqual(JSON.parse(result[0]), {"event_name":"changed","item":{"id":"item2"},"change":{"property":"testN2","new_value":"value3","old_value":"value1","new_timestamp":"2025-07-28T16:47:05.000Z"}});
+        assert.deepStrictEqual(JSON.parse(result[1]), {"event_name":"removed","item":{"id":"item1"}});
+
+    });
+
+    it('should not transmit automatically if there is nothing to send and empty transmissions are disallowed', function (done) {
+
+        pulsar.interval = 100; // Set a short interval for testing
+        pulsar.allow_empty = false; // Disable empty transmissions
+        pulsar.transmit = (payload) => { 
+            
+            // If we receive a payload, we fail the test
+            assert.fail(`the pulsar just sent a payload, but it should not have: ${payload}`);
+        };
+        pulsar.start(); // Start the Pulsar
+
+        // Wait 150ms to ensure no transmission occurs
+        setTimeout(() => {
+            pulsar.stop(); // Stop the Pulsar
+            done();
+        }, 150);
+    });
+
+    it('should transmit if there is something to send even if empty transmissions are disallowed', function (done) {
+
+        pulsar.interval = 100; // Set a short interval for testing
+        pulsar.allow_empty = false; // Disable empty transmissions
+        pulsar.transmit = (payload) => { 
+            assert.ok(payload.length > 0, 'the pulsar sent a payload, but it should not have been empty');
+            pulsar.stop(); // Stop the Pulsar after sending
+            done();
+        };
+        pulsar.start(); // Start the Pulsar
+
+        // Add an item to trigger a transmission
+        const item = new SynchroItem('item1');
+        ss.add(item);
+
     });
 
 });

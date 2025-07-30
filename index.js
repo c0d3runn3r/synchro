717948e8cbe0bd14292823e28f98570812bd4edd
c0d3runn3r <coderunner@novadynamics.com>
2025-07-29 17:32:39 -0700

    all tests passing

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const SynchroItem = require('./lib/SynchroItem');
 const SynchroSet = require('./lib/SynchroSet');
+const Pulsar = require('./lib/Pulsar');
+const Notion = require('./lib/Notion');
 
 module.exports = {
     SynchroItem,
     SynchroSet,
+    Pulsar,
+    Notion,
 };

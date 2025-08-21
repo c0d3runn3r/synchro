85cafc932c7fbcf1880e8e441c00312735fbf89b
c0d3runn3r <coderunner@novadynamics.com>
2025-08-21 07:37:14 -0700

    add export for server, client

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const SynchroItem = require('./lib/SynchroItem');
 const SynchroSet = require('./lib/SynchroSet');
 const Pulsar = require('./lib/Pulsar');
 const Notion = require('./lib/Notion');
+const DatastoreServer = require('./lib/DatastoreServer');
+const DatastoreClient = require('./lib/DatastoreClient');
 
 module.exports = {
     SynchroItem,
     SynchroSet,
     Pulsar,
     Notion,
+    DatastoreServer,
+    DatastoreClient
 };
